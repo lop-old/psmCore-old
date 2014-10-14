d8039da3189e7137ea6970119929d18c88564c20
lorenzop <lop@matt.lophome.com>
2014-10-14 00:56:58 -0400

    added module.class.php interface

---
+++ www/psmcore/portal/module.class.php
@@ -0,0 +1,11 @@
+<?php namespace psm\portal;
+if(!defined('psm\\PORTAL_LOADED') || \psm\PORTAL_LOADED !== TRUE) { echo '<header><meta http-equiv="refresh" content="1;url=../"></header>';
+	echo '<font size="+2" style="color: black; background-color: white;">Access Denied!!</font>'; exit(1); }
+interface module {
+
+	public function Render();
+
+	public function getDefaultPage();
+
+}
+?>
