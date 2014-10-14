a09601cfd68d4662784eabad1e2f58a9ddc7977b
lorenzop <lop@matt.lophome.com>
2014-10-13 23:49:08 -0400

    added comment header to core.php

--- www/psmcore/core.php
+++ www/psmcore/core.php
@@ -1,6 +1,28 @@
 <?php
+/**
+ * PSM Framework - ProSiteManager
+ * The main portal interface for the website. Everything starts and is managed from here.
+ *
+ * @copyright 2004-2014
+ * @license <<TBA>>
+ * @author lorenzo at PoiXson.com
+ * @link http://poixson.com/
+ */
 
-echo "THE TEST WORKS!";
+
+
+// defines for use in index.php
+// this will attempt to force the setting
+//define('psm\\DEBUG', TRUE);
+//define('psm\\MODULE', 'mod');
+//define('psm\\PAGE', 'home');
+
+
+
+// ==================================================
+// Please don't change anything below this line
+
+
 
 ?>
 ?>
