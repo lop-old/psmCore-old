d486ddfcb591e8cd04e54cfd1f3c250bc1137e3f
lorenzop <lop@matt.lophome.com>
2014-10-14 00:01:49 -0400

    set namespace to \psm in core.php default to display all errors until debug code loads prevent direct access to any php files other than index.php

--- www/psmcore/core.php
+++ www/psmcore/core.php
@@ -1,4 +1,4 @@
-<?php
+<?php namespace psm;
 /**
  * PSM Framework - ProSiteManager
  * The main portal interface for the website. Everything starts and is managed from here.
@@ -21,6 +21,24 @@
 
 // ==================================================
 // Please don't change anything below this line
+
+
+
+\error_reporting(\E_ALL);
+\ini_set('display_errors', 'On');
+\ini_set('html_errors',    'On');
+\ini_set('log_errors',     'On');
+\ini_set('error_log',      'psm_error.log');
+// prevent direct php file access
+if(defined('psm\\INDEX_DEFINE')) {
+	echo '<h2>Unknown state! core.php already loaded?</h2>';
+	exit(1);
+}
+if(defined('psm\\PORTAL_LOADED')) {
+	echo '<h2>Unknown state! portal already loaded?</h2>';
+	exit(1);
+}
+define('psm\\INDEX_DEFINE', TRUE);
 
 
 
