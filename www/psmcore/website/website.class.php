fd61e752684733777d5654c700ce6357ac0fde88
lorenzop <lop@matt.lophome.com>
2014-10-15 06:08:25 -0400

    added website abstract class

---
+++ www/psmcore/website/website.class.php
@@ -0,0 +1,12 @@
+<?php namespace psm;
+if(!defined('psm\\PORTAL_LOADED') || \psm\PORTAL_LOADED !== TRUE) { echo '<header><meta http-equiv="refresh" content="1;url=../"></header>';
+	echo '<font size="+2" style="color: black; background-color: white;">Access Denied!!</font>'; exit(1); }
+abstract class website {
+
+
+	public abstract function getName();
+	public abstract function getTitle();
+
+
+}
+?>
