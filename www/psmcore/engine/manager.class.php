507f7a16956876433869edd144873bfa0d36aeed
lorenzop <lop@matt.lophome.com>
2014-10-15 04:22:14 -0400

    added $head and $body variables to engine manager class using engine_head and engine_block classes manager implements engine

--- www/psmcore/engine/manager.class.php
+++ www/psmcore/engine/manager.class.php
@@ -1,20 +1,35 @@
 <?php namespace psm\engine;
 if(!defined('psm\\INDEX_DEFINE') || \psm\INDEX_DEFINE !== TRUE) { echo '<header><meta http-equiv="refresh" content="1;url=../"></header>';
 	echo '<font size="+2" style="color: black; background-color: white;">Access Denied!!</font>'; exit(1); }
-class manager {
+class manager implements \psm\engine {
 
 	private static $instance = NULL;
+
+	private $head;
+	private $body;
 
 	private $rendered = FALSE;
 
 
 
+	// single instance
 	public static function get() {
 		if(self::$instance == NULL)
 			self::$instance = new self();
 		return self::$instance;
 	}
 	protected function __construct() {
+		$this->head = engine_head::get();
+		$this->body = engine_block::get('body');
+	}
+
+
+
+	public function addHeader($html) {
+		$this->head->add($html);
+	}
+	public function add($html) {
+		$this->body->add($html);
 	}
 
 
