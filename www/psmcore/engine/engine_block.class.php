96772250a741cf0f2b8901435d56f689d18e2bf6
lorenzop <lop@matt.lophome.com>
2014-10-15 23:08:55 -0400

    fixed references and namespaces for engine and engine_interface

--- www/psmcore/engine/engine_block.class.php
+++ www/psmcore/engine/engine_block.class.php
@@ -1,7 +1,7 @@
 <?php namespace psm\engine;
 if(!defined('psm\\INDEX_DEFINE') || \psm\INDEX_DEFINE !== TRUE) { echo '<header><meta http-equiv="refresh" content="1;url=../"></header>';
 	echo '<font size="+2" style="color: black; background-color: white;">Access Denied!!</font>'; exit(1); }
-class engine_block implements \psm\engine {
+class engine_block implements \psm\engine\engine_interface {
 
 	protected static $instances = array();
 
--- www/psmcore/engine/engine_interface.class.php
+++ www/psmcore/engine/engine_interface.class.php
@@ -1,7 +1,7 @@
-<?php namespace psm;
+<?php namespace psm\engine;
 if(!defined('psm\\INDEX_DEFINE') || \psm\INDEX_DEFINE !== TRUE) { echo '<header><meta http-equiv="refresh" content="1;url=../"></header>';
 	echo '<font size="+2" style="color: black; background-color: white;">Access Denied!!</font>'; exit(1); }
-interface engine {
+interface engine_interface {
 
 
 	public function add($html);
