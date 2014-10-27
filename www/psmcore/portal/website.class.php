4d0d014659579c9afd483435728c6122ba276ebe
lorenzop <lop@matt.lophome.com>
2014-10-27 13:25:03 -0400

    changed website from interface to abstract class

--- www/psmcore/portal/website.class.php
+++ www/psmcore/portal/website.class.php
@@ -1,11 +1,12 @@
 <?php namespace psm\portal;
 if(!defined('psm\\PORTAL_LOADED') || \psm\PORTAL_LOADED !== TRUE) { echo '<header><meta http-equiv="refresh" content="1;url=../"></header>';
 	echo '<font size="+2" style="color: black; background-color: white;">Access Denied!!</font>'; exit(1); }
-interface website {
+abstract class website {
 
 
-	public function getName();
-	public function getTitle();
+
+	public abstract function siteName();
+	public abstract function siteTitle($title=NULL);
 
 
 }
