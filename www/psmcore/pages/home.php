99d51a57e18fbde65cf3d7dbe008d661c366bde0
Lop <lop@matt.lophome.com>
2014-11-02 22:10:58 -0500

    added page_interface.class.php and widget_interface.class.php files added default page home.php

---
+++ www/psmcore/pages/home.php
@@ -0,0 +1,8 @@
+<?php
+
+
+return 'Home page';
+
+
+?>
+
---
+++ www/psmcore/widgets/widget_interface.class.php
@@ -0,0 +1,12 @@
+<?php namespace psm\widgets;
+if(!defined('psm\\PORTAL_LOADED') || \psm\PORTAL_LOADED !== TRUE) { echo '<header><meta http-equiv="refresh" content="1;url=../"></header>';
+	echo '<font size="+2" style="color: black; background-color: white;">Access Denied!!</font>'; exit(1); }
+interface widget_interface {
+
+
+	public function render();
+	public function output();
+
+
+}
+?>
